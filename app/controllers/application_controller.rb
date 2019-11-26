b2cc795eb6fc02db1ce79e2fa9313378e09cbdd4
Juan Carlo Banayo <johncban@gmail.com>
2019-11-26 11:43:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,9 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/articles/:id' do
-    
+    @article = Article.find(params[:id])
+    @article.update(prams[:article])
+    redirect to "/articles/#{@article.id}"
   end
 
 
